a9c1d6c54c1505cbbdbfef80945e3f3a0b144d02
Guilherme de Campos Ribeiro <guilherme.campos.r@gmail.com>
2023-08-30 17:17:52 -0300

    Concluido os metodos GET de FILME

---
+++ senai_sprint2_api/api_filmes/webapi.filmes.tarde/Controllers/FilmeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.filmes.tarde.Domains;
+using webapi.filmes.tarde.Interfaces;
+using webapi.filmes.tarde.Repositories;
+
+namespace webapi.filmes.tarde.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    /// <summary>
+    /// Define qual é o tipo de resposta da API
+    /// </summary>
+    [Produces("application/json")]
+    public class FilmeController : ControllerBase
+    {
+
+        private IFilmeRepository _filmeRepository { get; set; }
+
+        public FilmeController()
+        {
+            _filmeRepository = new FilmeRepository();
+        }
+
+        /// <summary>
+        /// EndPoint que acessa um metodo de listar os filmes 
+        /// </summary>
+        /// <returns>Lista de filmes e um status code</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+
+                //Cria uma lista para receber os generos
+                List<FilmeDomain> ListaFilme = _filmeRepository.ListarTodos();
+
+                //retorna o STATUS CODE 200 OK, junto com a lista de generos no formato JSON
+                return Ok(ListaFilme);
+
+            }
+            catch (Exception erro)
+            {
+
+                //Retorna um status code 400, com uma mensagem de erro
+                return BadRequest(erro.Message);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Endpoint para buscar por id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                FilmeDomain filmeBuscado = _filmeRepository.BuscarPorId(id);
+
+                if (filmeBuscado == null)
+                {
+                    return NotFound("O genero buscado não foi encontrado");
+                }
+
+                return Ok(filmeBuscado);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+
+
+    }
+
+}
--- senai_sprint2_api/api_filmes/webapi.filmes.tarde/Repositories/FilmeRepository.cs
+++ senai_sprint2_api/api_filmes/webapi.filmes.tarde/Repositories/FilmeRepository.cs
@@ -1,38 +1,188 @@
-﻿using webapi.filmes.tarde.Domains;
+﻿using System.Data.SqlClient;
+using webapi.filmes.tarde.Domains;
 using webapi.filmes.tarde.Interfaces;
 
 namespace webapi.filmes.tarde.Repositories
 {
     public class FilmeRepository : IFilmeRepository
     {
+
+        /// <summary>
+        /// String de conexão com o banco de dados que recebe os seguintes parâmetros:
+        /// Data Source: Nome do servidor do banco
+        /// Initial Catalog: Nome do banco de dados
+        /// Autenticação:
+        ///     - Windows: Integrated Security = True
+        ///     - SqlServer: User Id = sa; Pwd = Senha
+        /// </summary>
+        private string StringConexao = "Data Source = NOTE10-S15; Initial Catalog = Filmes_Tarde; User Id = sa; Pwd = Senai@134";
+
+
         public void AtualizarIdCorpo(FilmeDomain filme)
         {
-            throw new NotImplementedException();
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string queryUpdate = "UPDATE Filme SET Titulo = '@TituloInserir', IdGenero = @IdGeneroInserir WHERE IdFilme = @IdFilme";
+
+                using (SqlCommand cmd = new SqlCommand(queryUpdate, con))
+                {
+                    cmd.Parameters.AddWithValue("@TituloInserir", filme.Titulo);
+                    cmd.Parameters.AddWithValue("@IdGeneroInserir", filme.IdGenero);
+                    cmd.Parameters.AddWithValue("@IdFilme", filme.IdFilme);
+
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void AtualizarIdUrl(int id, FilmeDomain filme)
         {
-            throw new NotImplementedException();
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string queryUpdateUrl = "UPDATE Filme SET Titulo = '@TituloInserir', IdGenero = @IdGeneroInserir WHERE IdFilme = @IdFilme";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(queryUpdateUrl, con))
+                {
+                    cmd.Parameters.AddWithValue("@Titulo", filme.Titulo);
+                    cmd.Parameters.AddWithValue("@IdGeneroInserir", filme.IdGenero);
+                    cmd.Parameters.AddWithValue("@IdFilme", id);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+
+
+            }
+
         }
 
         public FilmeDomain BuscarPorId(int id)
         {
-            throw new NotImplementedException();
+            FilmeDomain filmeBuscado = new FilmeDomain();
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                // string querySelectFilme = "SELECT IdFilme, IdGenero, Titulo FROM Filme WHERE IdFilme = @IdFilme";
+
+                string querySelectFilme = "SELECT IdFilme, Filme.IdGenero, Titulo, Genero.Nome FROM Filme inner join Genero on Filme.IdGenero = Genero.IdGenero Where IdFilme = @IdFilme";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(querySelectFilme, con))
+                {
+                    cmd.Parameters.AddWithValue("@IdFilme", id);
+
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        if (rdr.Read())
+                        {
+                            filmeBuscado = new FilmeDomain()
+                            {
+                                IdFilme = Convert.ToInt32(rdr["IdFilme"]),
+                                IdGenero = Convert.ToInt32(rdr["IdGenero"]),
+                                Titulo = rdr["Titulo"].ToString(),
+
+                                Genero = new GeneroDomain() 
+                                { 
+                                    IdGenero = Convert.ToInt32(rdr["IdGenero"]),
+                                    Nome = rdr["Nome"].ToString(),
+                                }
+                            };
+                        }
+                    }
+                }
+            }
+            return filmeBuscado;
         }
 
         public void Cadastrar(FilmeDomain novoFilme)
         {
-            throw new NotImplementedException();
+
+            // Declara a SqlConnection passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                // Declara a instrução a ser executada passando como valor do nome o objeto
+                string queryInsert = "INSERT INTO Filme (IdGenero, Titulo) VALUES (@IdGenero, '@Titulo')";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con))
+                {
+                    cmd.Parameters.AddWithValue("@IdGenero", novoFilme.IdGenero);
+                    cmd.Parameters.AddWithValue("@Titulo", novoFilme.Titulo);
+                    //executa a querry
+                    cmd.ExecuteNonQuery();
+
+                }
+
+            }
         }
 
         public void Deletar(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string queryDelete = $"DELETE FROM Filme WHERE IdFilme = {id}";
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(queryDelete, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
         }
 
         public List<FilmeDomain> ListarTodos()
         {
-            throw new NotImplementedException();
+
+            // Cria uma lista de gêneros onde os gêneros serão armazenados
+            List<FilmeDomain> listaFilmes = new List<FilmeDomain>();
+
+            // Declara a SqlConnection passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                // Declara a instrução a ser executada
+                string querySelectAll = "Select Filme.IdFilme,Filme.IdGenero, Genero.Nome AS Genero, Titulo From Filme Inner Join Genero ON  Genero.IdGenero = Filme.IdGenero";
+
+                // Abre a conexão com o banco de dados
+                con.Open();
+
+                // Declara o SqlDataReader para percorrer(ler) a tabela no banco de dados
+                SqlDataReader rdr;
+
+                // Declara o SqlCommand passando a query que será executada e a conexão
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    // Executa a query e armazena os dados no rdr
+                    rdr = cmd.ExecuteReader();
+
+                    // Enquanto houver registros para serem lidos no rdr, o laço se repetirá
+                    while (rdr.Read())
+                    {
+                        FilmeDomain filme = new FilmeDomain()
+                        {
+                            // Atribui a propriedade IdGenero o valor da primeira coluna da tabela
+                            IdFilme = Convert.ToInt32(rdr[0]),
+                            IdGenero = Convert.ToInt32(rdr[1]),
+                            Titulo = rdr[3].ToString(),
+
+                            Genero = new GeneroDomain()
+                            {
+                                IdGenero = Convert.ToInt32(rdr[1]),
+                                Nome = rdr[2].ToString(),
+                            }
+                        };
+                        listaFilmes.Add(filme);
+                    }
+                }
+            }
+            return listaFilmes;
         }
     }
 }
