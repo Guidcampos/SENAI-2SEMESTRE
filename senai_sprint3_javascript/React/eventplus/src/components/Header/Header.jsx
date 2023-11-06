3a4664fe02037d81c6f1b673ac90ea9c7d72fd2f
Guilherme de Campos Ribeiro <guilherme.campos.r@gmail.com>
2023-11-06 17:19:33 -0300

    REACT: Feito o CSS do HEADER e NAV, iniciao o CSS-Title

--- senai_sprint3_javascript/React/eventplus/src/components/Header/Header.jsx
+++ senai_sprint3_javascript/React/eventplus/src/components/Header/Header.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
-import Container  from '../Container/Container';
+import Container from '../Container/Container';
 import Nav from '../Nav/Nav'
 import PerfilUsuario from '../PerfilUsuario/PerfilUsuario'
-import menuBar from '../../Assets/images/images/menubar.png'
+import menuBar from '../../Assets/images/menubar.png'
 
 const Header = () => {
+    const [exibeNavbar, setExibeNavbar] = useState(false);
+    console.log(exibeNavbar);
+
     return (
-        <header className='headerpage'>
-           <Container> 
-                <div className='header-flex'>
-                    <img src={menuBar} alt="Imagem menu de barras. Serve para exibir ou esconder o menu no smartphone" />
 
+        <header className='headerpage '>
+            <Container >
+                <div className='header-flex '>
+                    <img
+                        src={menuBar}
+                        alt="Imagem menu de barras. Serve para exibir ou esconder o menu no smartphone"
+                        onClick={() => setExibeNavbar(true)}
+                    />
+
+
+                    <Nav 
+                    exibeNavbar = {exibeNavbar}
+                    setExibeNavbar = {setExibeNavbar} />
+                    <PerfilUsuario />
                 </div>
-
-                <Nav/>
-                <PerfilUsuario/>
-           </Container>
+            </Container>
 
 
         </header>
--- senai_sprint3_javascript/React/eventplus/src/components/Nav/Nav.jsx
+++ senai_sprint3_javascript/React/eventplus/src/components/Nav/Nav.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Nav.css'
 import { Link } from 'react-router-dom';
-import logoMobile from '../../Assets/images/images/logo-white.svg'
-import logoDesktop from '../../Assets/images/images/logo-pink.svg'
+import logoMobile from '../../Assets/images/logo-white.svg'
+import logoDesktop from '../../Assets/images/logo-pink.svg'
 
-const Nav = () => {
+const Nav = ({setExibeNavbar, exibeNavbar}) => {
     return (
-        <nav className='navbar'>
-            <span className='navbar__close'></span>
-            <Link to = "/">
-                <img src={window.innerHeight >= 992 ? logoDesktop : logoMobile} className='eventlogo__logo-image' alt="Event plus Logo" />
+        <nav className= { `navbar ${exibeNavbar ? "exibeNavbar" : ""}`}>
+            <span className='navbar__close ' onClick={() => setExibeNavbar(false)}>x</span>
+            <Link to="/">
+                <img src={window.innerWidth >= 992 ? logoDesktop : logoMobile}
+                    className='eventlogo__logo-image '
+                    alt="Event plus Logo"
+                />
             </Link>
 
             <div className='navbar__items-box'>
-                <Link to = "/">Home</Link>
-                <Link to = "/tipo-eventos">Tipo Eventos</Link>
-                <Link to = "/eventos">Eventos</Link>
-                <Link to = "/login">Login</Link>
+                <Link to="/" className='navbar__item'>Home</Link>
+                <Link to="/tipo-eventos" className='navbar__item'>Tipo Eventos</Link>
+                <Link to="/eventos" className='navbar__item'>Eventos</Link>
+                <Link to="/login" className='navbar__item'>Login</Link>
             </div>
 
         </nav>
