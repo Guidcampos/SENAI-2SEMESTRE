78968bde4081f797a5209a35d426ab079abb3b74
Guilherme de Campos Ribeiro <guilherme.campos.r@gmail.com>
2023-11-10 17:17:20 -0300

    REACT: Tooltip, axios para services

---
+++ senai_sprint3_javascript/React/eventplus/src/Utils/stringFunction.js
@@ -0,0 +1,14 @@
+/**
+ * funçao para inverter a data para padrao brasileiro
+ * @param {*} data 
+ * @returns 
+ */
+export const dateFormatDbToView = data => {
+    
+    data = data.substr(0,10);//retorna apenas a data[2023-11-10]
+    data = data.split("-") //retorna um array [2023, 11, 10]
+
+    
+    return `${data[2]}/${data[1]}/${data[0]}`;
+}
+
--- senai_sprint3_javascript/React/eventplus/src/pages/HomePage/HomePage.jsx
+++ senai_sprint3_javascript/React/eventplus/src/pages/HomePage/HomePage.jsx
@@ -7,32 +7,28 @@ import MainContent from '../../components/MainContent/MainContent'
 import ContactSection from '../../components/ContactSection/ContactSection'
 import NextEvent from '../../components/NextEvent/NextEvent'
 import Container from '../../components/Container/Container'
-import { Axios } from 'axios';
+import  api  from '../../Services/Service';
 
 const HomePage = () => {
+    const [nextEvents, setNextEvents] = useState([]);
 
     useEffect( () => {
         //chamar a api
        async function getProximosEventos() {
             try {
-                const promise = await Axios.get('https://localhost:7118/api/Evento/ListarProximos')
-                console.log(promise.data)
-                setNextEvents(promise.data)
+                const promise = await api.get( "/Evento/ListarProximos")
+                console.log(promise.data);
+                setNextEvents(promise.data);
+                
             } catch (error) {
                 alert("Deu ruim na API")
             }
-            
         }
-    },[]
+        getProximosEventos();
+        console.log("A HOME FOI MONTADA!");
+    },[]);
 
-    );
-
-    //FAKE MOCK -- API MOCADA
-    const [nextEvents, setNextEvents] = useState([
-        { id: 1, title: 'Evento teste', descricao: "Evento para testar os cards", data: "10/11/2023" },
-        { id: 2, title: 'Evento teste 2', descricao: "Segundo Evento para testar os cards", data: "11/11/2023" }
-
-    ]);
+   
 
 
 
@@ -45,15 +41,15 @@ const HomePage = () => {
                 <Container>
                     <Title titleText={"Próximos Eventos"} />
                     <div className="events-box">
-
+                            
                         {
                             nextEvents.map((e) => {
                                 return (
                                     <NextEvent
-                                        idEvento={e.id}
-                                        title={e.title}
+                                        idEvento={e.idEvento}
+                                        title={e.nomeEvento}
                                         description={e.descricao}
-                                        eventDate={e.data}
+                                        eventDate={e.dataEvento}
 
                                     />)
                             })
