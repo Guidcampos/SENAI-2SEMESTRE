09efc48f6e9955b2a0a0556ea40d6d1bb24772b4
Guilherme de Campos Ribeiro <guilherme.campos.r@gmail.com>
2023-11-16 17:18:55 -0300

    REACT: Iniciado o componente de TABLETP

--- senai_sprint3_javascript/React/eventplus/src/pages/TipoEventosPage/TipoEventosPage.jsx
+++ senai_sprint3_javascript/React/eventplus/src/pages/TipoEventosPage/TipoEventosPage.jsx
@@ -7,42 +7,65 @@ import Container from '../../components/Container/Container';
 import eventTypeImage from '../../Assets/images/tipo-evento.svg'
 import { Input, Button } from "../../components/FormComponents/FormComponents";
 import api from "../../Services/Service"
+import TableTp from './TableTp/TableTp';
 
 const TipoEventos = () => {
     const [titulo, setTitulo] = useState("");
-    const [frmEdit, setfrmEdit] = useState(false);
+    const [frmEdit, setFrmEdit] = useState(false);
+    const [tipoEventos, setTipoEventos] = useState ([
+        {idTipoEvento: "123", titulo: "Show"},
+        {idTipoEvento: "321", titulo: "Show 2"},
+        {idTipoEvento: "567", titulo: "Show 3"}
+    
+    ]);
+
+
+    async function handleSubmit(e) {
+         //para o submit do formulario
+         e.preventDefault();
+         //validar pelo menos 3 caracteres 
+         if (titulo.trim().length < 3) {
+             alert("O titulo deve ter no minimo 3 caracteres")
+             return;
+         }
+ 
+         try {
+             const retorno = await api.post("/TiposEvento", {titulo:titulo})
+             console.log("Cadastrado com sucesso");
+             console.log(retorno.data);
+             setTitulo("");
+ 
+         } catch (error) {
+           console.log("deu ruim na api");  
+           console.log(error);  
+         }
+ 
+         //chamar a api
+ 
+ 
+     }
 
     function handleUpdate() {
         alert("Bora atualizar")
     }
-   async function handleSubmit(e) {
-        //para o submit do formulario
-        e.preventDefault();
-        //validar pelo menos 3 caracteres 
-        if (titulo.trim().length < 3) {
-            alert("O titulo deve ter no minimo 3 caracteres")
-            return;
-        }
 
-        try {
-            const retorno = await api.post("/TiposEvento", {titulo:titulo})
-            console.log("Cadastrado com sucesso");
-            console.log(retorno.data);
-            setTitulo("");
+    function showUpdateForm () {
+        alert("MOSTRAR MENU DE ATUALIZACAO")
+    }
+   
+    function editActionAbort () {
+        alert("cancelar edição")
+    }
 
-        } catch (error) {
-          console.log("deu ruim na api");  
-          console.log(error);  
-        }
-
-        //chamar a api
-
+    function handleDelete () {
+        alert("DELETAR")
 
     }
 
 
     return (
         <MainContent>
+            {/* CADASTRO DE TIPO DE USUARIOS  */}
             <section className='cadastro-evento-section'>
 
                 <Container>
@@ -102,6 +125,19 @@ const TipoEventos = () => {
 
             </section>
 
+            {/* LISTAGEM DE TIPO DE USARIOS  */}
+            <section className="lista-eventos-section">
+                <Container>
+                    <Title titleText={"Lista de Tipo de Eventos"} color='white'/>
+                    <TableTp 
+                        dados = {tipoEventos}
+                        fnUpdate={showUpdateForm}
+                        fnDelete={handleDelete}
+                    />                
+
+                </Container>
+                                
+            </section>                        
         </MainContent>
 
     );
